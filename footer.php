95072f35d7b9e42c40b7730578a528c7d18f3ae0
Manolo Martinez <141935544+Creepermanolo@users.noreply.github.com>
2026-09-08 11:10:10 +0200

    dth

---
+++ footer.php
@@ -0,0 +1,8 @@
+<!-- footer.php -->
+<!-- footer.php -->
+<footer class="bg-light text-center text-lg-start mt-auto">
+  <div class="text-center p-3">
+    © 2026 Copyright:
+    <a class="text-dark">Sylvain-Pierre Durif, Grand Monarque, Christ Cosmique, Homme vert, Merlin l'Enchanteur, Orianna, Messie et le Roi, le Pape Pierre, Le Présidence de la gouvernance pacifique Planétaire Elvita</a>
+  </div>
+</footer>
