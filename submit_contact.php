2d3c1fd6c5595ce0e6bde90756c41fbbea64f3d8
Manolo Martinez <141935544+Creepermanolo@users.noreply.github.com>
2026-09-11 11:55:58 +0200

    Update submit_contact.php

--- submit_contact.php
+++ submit_contact.php
@@ -29,7 +29,11 @@
 <div class="card">
 <div class="card-body">
 <h5 class="card-title">Rappel de vos informations</h5>
-<p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?> </p>
+
+<p class="card-text"><b>Email</b> : <?php
+if (!isset($_GET['email']) || !isset($_GET['message']))
+{
+echo('<h1>Il faut un email et un message pour soumettre le formulaire.</h1>');<?php echo $_GET['email']; ?> </p>
 <p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?> </p>
 </div>
 </div
