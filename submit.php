008d5860e3bb687706cec7ea1d97398b4856103e
Manolo Martinez <141935544+Creepermanolo@users.noreply.github.com>
2026-09-11 09:01:13 +0200

    dddfdf

---
+++ submit.php
@@ -0,0 +1,8 @@
+<h1>Message bien reçu !</h1>
+<div class="card">
+<div class="card-body">
+<h5 class="card-title">Rappel de vos informations</h5>
+<p class="card-text"><b>Email</b> : <?php echo $_GET['email']; ?> </p>
+<p class="card-text"><b>Message</b> : <?php echo $_GET['message']; ?> </p>
+</div>
+</div
